efeabcefbfc5ebe1af5bd19c653fa1ab975b5544
Jordan Yen <Jordan Yen>
2017-02-11 17:53:23 -0800

    fixing reset bug

--- app/script.js
+++ app/script.js
@@ -6,7 +6,7 @@ var main = function(){
             dataType: 'json',
             success: function(data) {
                 for(var k = 0; k < data.length; k++){
-                    var html = $('<li>').text(data[k].comment);
+                    var html = $('<li>').text(data[k].comment).addClass("newComment");
                     html.prependTo('#comments'); 
                 }
             }
